0e9ff41db820a60b302777607acd118326e8683e
fghjjkl32 <fghjjkl32@naver.com>
2021-08-10 07:31:25 +0900

    setting

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -22,7 +22,7 @@ class WeatherModel {
   String getMessage(int temp) {
     if (temp > 25) {
       return 'It\'s 🍦 time';
-    } else if (temp > 20) {
+    } else if (temp > 20)
       return 'Time for shorts and 👕';
     } else if (temp < 10) {
       return 'You\'ll need 🧣 and 🧤';
